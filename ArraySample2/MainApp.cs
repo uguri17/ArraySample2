33d1bdfd26deab6489e02adc8b607115f6bf3d18
RND-HW3 <RND-HW3@RND-HW3>
2025-04-16 09:11:55 +0900

    초기 커밋.

---
+++ ArraySample2/MainApp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace ArraySample2
+{
+    internal class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int[] scores = new int[5];
+            scores[0] = 80;
+            scores[1] = 74;
+            scores[2] = 81;
+            scores[^2] = 90;
+            scores[^1] = 34;            
+            // 80 74 81 90 34
+            // 0  1  2  ^2 ^1
+
+            int sum = 0;
+            foreach (int score in scores)
+            {
+                Console.WriteLine(score);
+                sum += score;                
+            }
+            
+            Console.WriteLine(sum);
+        }
+    }
+}
